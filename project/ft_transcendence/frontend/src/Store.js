ff9498962f8f74dcfa3fe3c12c50dcfdf91926e5
Maxime Uteza <muteza@c2r11p3.42nice.fr>
2024-08-28 18:32:43 +0200

    working front

--- project/ft_transcendence/frontend/src/Store.js
+++ project/ft_transcendence/frontend/src/Store.js
@@ -1,33 +1,33 @@
-export class Store {
-    constructor(initialState = {}) {
-        this.state = initialState;
-        this.listeners = [];
-    }
+// export class Store {
+//     constructor(initialState = {}) {
+//         this.state = initialState;
+//         this.listeners = [];
+//     }
 
-    getState() {
-        return this.state;
-    }
+//     getState() {
+//         return this.state;
+//     }
 
-    subscribe(listener) {
-        this.listeners.push(listener);
-    }
+//     subscribe(listener) {
+//         this.listeners.push(listener);
+//     }
 
-    setState(newState) {
-        this.state = { ...this.state, ...newState };
-        this.notify();
-    }
+//     setState(newState) {
+//         this.state = { ...this.state, ...newState };
+//         this.notify();
+//     }
 
-    notify() {
-        this.listeners.forEach(listener => listener(this.state));
-    }
-}
+//     notify() {
+//         this.listeners.forEach(listener => listener(this.state));
+//     }
+// }
 
-/*
-// Example usage:
-const appStore = new Store({ count: 0, user: { name: 'John Doe' } });
+// /*
+// // Example usage:
+// const appStore = new Store({ count: 0, user: { name: 'John Doe' } });
 
-// Subscribe a component to the store
-appStore.subscribe((state) => {
-    console.log('State updated:', state);
-});
-*/
+// // Subscribe a component to the store
+// appStore.subscribe((state) => {
+//     console.log('State updated:', state);
+// });
+// */
