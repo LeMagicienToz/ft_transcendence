21591144cf530dbe65fb85b9d79b9a6ef77f9b55
pory <philemon.ory84@gmail.com>
2024-09-23 16:13:40 +0200

    fix

--- project/backend/authentification/authentification/urls.py
+++ project/backend/authentification/authentification/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from myauth.views import login_42, callback_42
+from auth_app.views import login_42, callback_42
 
 urlpatterns = [
     path('admin/', admin.site.urls),
