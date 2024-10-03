33f0b83a22c58373bd3b37f4b761464dd607ccb5
pory <philemon.ory84@gmail.com>
2024-10-03 17:09:52 +0200

    back update

--- project/backend/authentification/auth_app/views.py
+++ project/backend/authentification/auth_app/views.py
@@ -8,31 +8,36 @@ from django.contrib.auth.models import User
 from .decorators import jwt_required
 import jwt, time, requests
 from django.conf import settings
+import os
 
 
 
 def login_42(request):
-    login_url = f"{AUTH_URL}?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code"
+    login_url = f'{os.getenv("AUTH_URL")}?client_id={os.getenv("API_42_CLIENT_ID")}&redirect_uri={os.getenv("CALLBACK_URL_YOU_SET_ON_42")}&response_type=code'
     return redirect(login_url)
 
 def callback_42(request):
     code = request.GET.get('code')
     
     token_data = {
-        'grant_type': 'authorization_code',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
+        'grant_type': 'client_credentials',
+        'client_id': os.getenv('API_42_CLIENT_ID'),
+        'client_secret': os.getenv('API_42_CLIENT_SECRET'),
         'code': code,
-        'redirect_uri': REDIRECT_URI,
+        'redirect_uri': os.getenv('CALLBACK_URL_YOU_SET_ON_42')
     }
     
-    token_response = requests.post(TOKEN_URL, data=token_data)
+    token_response = requests.post(os.getenv('TOKEN_URL'), data=token_data)
     token_json = token_response.json()
     access_token = token_json.get('access_token')
 
     if access_token:
         headers = {'Authorization': f'Bearer {access_token}'}
-        user_response = requests.get(USER_URL, headers=headers)
+        user_response = requests.get(header=header, os.getenv('USER_URL'))
+
+        return user_response.json()
+        # print(f"Status Code: {user_response.status_code}")
+        # print(f"Response Content: {user_response.text}")
         user_data = user_response.json()
 
         username = user_data.get('login') + '#42'
@@ -155,3 +160,22 @@ def delete_account(request):
     user.delete()
     return JsonResponse({'success': True,'message': 'Compte supprimé avec succès'}, status=200)
 
+
+def send_user_info(user):
+    api_key = os.getenv('GAME_API_KEY')
+    
+    headers = {
+        'Authorization': f'Api-Key {api_key}',
+        'Content-Type': 'application/json'
+    }
+    
+    data = {
+        'user_id': user.id,
+        'username': user.username,
+        'profile_picture_url': user.customuser.profile_picture_url
+
+
+    }
+    response = requests.post('http://game:8001/api/game/users/', headers=headers, data=json.dumps(data))
+    
+    return response.json()
--- project/backend/game/game_app/views.py
+++ project/backend/game/game_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+@require_POST
+def receive_user(request):
+        return JsonResponse({'received': 'ok'})
