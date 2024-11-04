2789f660f1cc88ee996f4d6976bd2a04cbaca342
raphael <rdmrdm3@gmail.com>
2024-11-04 11:21:38 +0100

    removed method test (allready done by APIview) + cleaning

--- project/backend/game/game_app/views.py
+++ project/backend/game/game_app/views.py
@@ -16,31 +16,28 @@ def ping(request):
 
 class GameCreateView(APIView):
     def post(self, request):
-        # Vérification du type de requête
-        if request.method != 'POST':
-            return JsonResponse({'error': 'Method not allowed'}, status=405)
-        # Récupérer les données du joueur 1
+        # get player 1 user_id and user_name
         try:
             player1_user_id = int(request.POST.get('user_id'))
         except (ValueError, TypeError):
             return JsonResponse({'error': 'Invalid user ID'}, status=400)
         player1_user_name = request.POST.get('user_name')
-        # Récupérer le type de jeu (1 vs 1 ou 2 vs 2)
+        # get game type (1 vs 1 or 2 vs 2)
         match_type = request.POST.get('match_type')
         if not match_type:
             match_type = '1v1'
         elif match_type not in ['1v1', '2v2']:
             return JsonResponse({'error': 'Invalid match type'}, status=400)
-        # Récupérer le type de jeu (pong ou snake)
+        # get game name (pong or snake)
         game_type = request.POST.get('game_type')
         if not game_type:
             game_type = 'pong'
         elif game_type not in ['pong', 'snake']:
             return JsonResponse({'error': 'Invalid game type'}, status=400)
-        # Validation data
+        # Data validation
         if not player1_user_id or not player1_user_name:
             return JsonResponse({'error': 'Player information is required'}, status=400)
-        # Initialiser le jeu avec les informations du joueur 1
+        # Game init with palyer 1
         try:
             game = Game.objects.create(
                 player1_user_id=player1_user_id,
@@ -55,14 +52,12 @@ class GameCreateView(APIView):
 
 class GameListView(APIView):
     def get(self, request):
-        if request.method != 'GET':
-            return JsonResponse({'error': 'Method not allowed'}, status=405)
         games = Game.objects.all().values(
             'id',
             'status',
             'game_type',
             'match_type',
-            'player1_user_id',   # eventuellement supprimer id et name si GPRD
+            'player1_user_id',   # maybe remove id and name if GPRD
             'player1_user_name',
             'player1_score',
             'player1_nickname',
@@ -83,8 +78,6 @@ class GameListView(APIView):
 
 class GameDetailView(APIView):
     def get(self, request, game_id):
-        if request.method != 'GET':
-            return JsonResponse({'error': 'Method not allowed'}, status=405)
         game = get_object_or_404(Game, id=game_id)
         game_details = {
             'game_id': game.id,
@@ -112,42 +105,39 @@ class GameDetailView(APIView):
 
 class GameJoinView(APIView):
     def put(self, request, game_id):
-        if request.method != 'PUT':
-            return JsonResponse({'error': 'Method not allowed'}, status=405)
-        # Récupérer la partie en cours
+        # get game id
         game = get_object_or_404(Game, id=game_id)
         try:
             data = json.loads(request.body)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON format'}, status=400)
-        player2_user_id = data.get('user_id')
-        player2_user_name = data.get('user_name')
-        # Validation des informations du joueur 2
-        if not player2_user_id or not player2_user_name:
-            return JsonResponse({'error': 'Player information is required'}, status=400)
-        # Validation ID du joueur 2
+        # get player 1 user_id and user_name
         try:
-            player2_user_id = int(player2_user_id)
+            player2_user_id = int(data.get('user_id'))
         except (ValueError, TypeError):
             return JsonResponse({'error': 'Invalid user ID'}, status=400)
-        # Vérifier que ce joueur n'est pas déjà dans la partie
+        player2_user_name = data.get('user_name')
+        # Player 2 info validation
+        if not player2_user_id or not player2_user_name:
+            return JsonResponse({'error': 'Player information is required'}, status=400)
+        # Check player not allready in the game
         if player2_user_id in [game.player1_user_id, game.player2_user_id, game.player3_user_id, game.player4_user_id]:
             return JsonResponse({'error': 'Player has already joined the game'}, status=400)
-        # Vérifier si le type de match est valide
+        # check valid match type
         if game.match_type not in ['1v1', '2v2']:
             return JsonResponse({'error': 'Invalid match type'}, status=400)
         if game.match_type == '1v1':
-            # Vérifier si player2 est déjà assigné
+            # check if game is full
             if game.player2_user_id != 0:
                 return JsonResponse({'message': 'Game is already full'}, status=400)
-            # Assignation des données au joueur 2
+            # Assign player 2
             game.player2_user_id = player2_user_id
             game.player2_user_name = player2_user_name
         elif game.match_type == '2v2':
-            # Vérifier si les joueurs 2, 3 et 4 sont assignés
+            # check if game is full
             if game.player2_user_id != 0 and game.player3_user_id != 0 and game.player4_user_id != 0:
                 return JsonResponse({'message': 'Game is already full'}, status=400)
-            # Assignation des données au joueur 2 ou 3 ou 4
+            # Assign player 2
             if game.player2_user_id == 0:
                 game.player2_user_id = player2_user_id
                 game.player2_user_name = player2_user_name
@@ -162,25 +152,23 @@ class GameJoinView(APIView):
 
 class GameStartView(APIView):
     def post(self, request, game_id):
-        if request.method != 'POST':
-            return JsonResponse({'error': 'Method not allowed'}, status=405)
-        # Récupérer la partie à démarrer
+        # get game_id
         game = get_object_or_404(Game, id=game_id)
-        # Vérifier si le jeu est en attente et a le bon type de match
-        #if game.status != 'waiting':
-        #    return JsonResponse({'message': 'Game cannot be started'}, status=400)
+        # check if game is waiting, then match type
+        if game.status != 'waiting':
+            return JsonResponse({'message': 'Game cannot be started'}, status=400)
         if game.match_type not in ['1v1', '2v2']:
             return JsonResponse({'error': 'Invalid match type'}, status=400)
+        # check if game is full : 2 players in 1v1 and 4 in 2v2
         active_players = (game.player1_user_id != 0) + (game.player2_user_id != 0) + (game.player3_user_id != 0) + (game.player4_user_id != 0)
         if game.match_type == '1v1' and active_players != 2:
             return JsonResponse({'error': 'A 1v1 game requires exactly 2 players'}, status=400)
         elif game.match_type == '2v2' and active_players != 4:
             return JsonResponse({'error': 'A 2v2 game requires exactly 4 players'}, status=400)
-        # Changer le statut du jeu et enregistrer l'heure de début
+        # Change status to playing and setup start_time
         game.status = 'playing'
         game.start_time = timezone.now()
         game.save()
-        # ask the async game consumer to start a loop
         # Send a message to the GameConsumer to start the game loop
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
