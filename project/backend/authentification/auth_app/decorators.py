4f0a82bac51f9b0dc17cdb47808951776c1ef5fc
pory <philemon.ory84@gmail.com>
2024-11-11 13:46:22 +0100

    fix gestion jwt expired

--- project/backend/authentification/auth_app/decorators.py
+++ project/backend/authentification/auth_app/decorators.py
@@ -13,42 +13,38 @@ def jwt_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         access_token = request.COOKIES.get('token')
         refresh_token = request.COOKIES.get('refresh_token')
-       
+
         if access_token:
             try:
                 payload = jwt.decode(access_token, settings.SECRET_KEY, algorithms=['HS256'])
-                try:
-                    request.user = User.objects.get(id=payload['user_id'])
-                    redis_token = r.get(f'user_{request.user.id}_token')
-                    if redis_token and redis_token.decode() != access_token:
-                        return JsonResponse({'success': False, 'error': 'Access token revoqué'}, status=401)
-                except User.DoesNotExist:
-                    return JsonResponse({'success': False, 'error': 'Utilisateur non trouvé'}, status=404)
+                request.user = User.objects.get(id=payload['user_id'])
+                redis_token = r.get(f'user_{request.user.id}_token')
+                if redis_token and redis_token.decode() != access_token:
+                    return JsonResponse({'success': False, 'error': 'Access token révoqué'}, status=401)
                 return view_func(request, *args, **kwargs)
 
             except jwt.ExpiredSignatureError:
-                if refresh_token:
-                    try:
-                        refresh_payload = jwt.decode(refresh_token, settings.REFRESH_TOKEN_SECRET, algorithms=['HS256'])
-                        request.user = User.objects.get(id=refresh_payload['user_id'])
-                        redis_refresh_token = r.get(f'user_{request.user.id}_refresh_token')
-                        if redis_refresh_token and redis_refresh_token.decode() != refresh_token:
-                            return JsonResponse({'success': False, 'error': 'Refresh token revoqué'}, status=401)
-                        new_access_token = jwt.encode({'user_id': refresh_payload['user_id'], 'iat': int(time.time()), 'exp': int(time.time()) + 60 * 5}, settings.SECRET_KEY, algorithm='HS256')
-                        response = view_func(request, *args, **kwargs)
-                        response.set_cookie('token', new_access_token, max_age=60*5, httponly=True, secure=True, samesite='Strict')
-                        return response
-
-                    except jwt.ExpiredSignatureError:
-                        return JsonResponse({'success': False, 'error': 'Refresh token expire'}, status=401)
-                    except jwt.InvalidTokenError:
-                        return JsonResponse({'success': False, 'error': 'Refresh token invalide'}, status=400)
-                else:
-                    return JsonResponse({'success': False, 'error': 'Access token expire et refresh token manquand'}, status=401)
+                pass
             except jwt.InvalidTokenError:
                 return JsonResponse({'success': False, 'error': 'Access token invalide'}, status=400)
-        else:
-            return JsonResponse({'success': False, 'error': 'Access token manquand'}, status=400)
+
+        if refresh_token:
+            try:
+                refresh_payload = jwt.decode(refresh_token, settings.REFRESH_TOKEN_SECRET, algorithms=['HS256'])
+                request.user = User.objects.get(id=refresh_payload['user_id'])
+                redis_refresh_token = r.get(f'user_{request.user.id}_refresh_token')
+                if redis_refresh_token and redis_refresh_token.decode() != refresh_token:
+                    return JsonResponse({'success': False, 'error': 'Refresh token révoqué'}, status=401)
+                new_access_token = jwt.encode({'user_id': refresh_payload['user_id'], 'iat': int(time.time()), 'exp': int(time.time()) + 60 * 5}, settings.SECRET_KEY, algorithm='HS256')
+                response = view_func(request, *args, **kwargs)
+                response.set_cookie('token', new_access_token, max_age=60*5, httponly=True, secure=True, samesite='Strict')
+                return response
+
+            except jwt.ExpiredSignatureError:
+                return JsonResponse({'success': False, 'error': 'Refresh token expiré'}, status=401)
+            except jwt.InvalidTokenError:
+                return JsonResponse({'success': False, 'error': 'Refresh token invalide'}, status=400)
+        return JsonResponse({'success': False, 'error': 'Accès refusé : jetons manquants ou expirés'}, status=401)
 
     return _wrapped_view
 
