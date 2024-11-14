702943672238df0f8bcccf5e9bdfd80c560c2898
Maxime Uteza <muteza@c3r1p2.42nice.fr>
2024-11-14 16:24:47 +0100

    create game finish error davatar

---
+++ project/frontend/src/game/WaitingRoom.jsx
@@ -0,0 +1,14 @@
+import './Tableone.css';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyButton from '../Theme/MyButton';
+
+const WaitingRoom = () => {
+    return(
+        <div>
+            dkwopakdwao
+        </div>
+    )
+}
+
+export default WaitingRoom;
