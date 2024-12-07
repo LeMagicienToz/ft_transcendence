c7a07b690437f0aaffdd388737d1dcd9697a5fa5
Raphael Di marzio <rdi-marz@student.42nice.fr>
2024-12-07 14:25:11 +0100

    comment unused endpoints

--- services/backend/api_game/service/app/urlpatterns.py
+++ services/backend/api_game/service/app/urlpatterns.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .endpoints import endpoints
 
 from .endpoints.endpoints import (
-    GameCreateView, ListView, GameDetailView, GameJoinView, GameUserHistoryView, GameDeleteView,
-	TournamentCreateView, TournamentDetailView, TournamentJoinView,
-	TournamentDeleteView, TournamentListView
+    GameCreateView, ListView, GameDetailView, GameJoinView, GameUserHistoryView,
+	TournamentCreateView, TournamentDetailView, TournamentJoinView
 )
 
 # TODO remove this
@@ -20,15 +19,15 @@ urlpatterns = [
     path("game-details/<int:game_id>/", GameDetailView.as_view(), name="game-detail"),   # Detail one game
     path("join/<int:game_id>/", GameJoinView.as_view(), name="game-join"),  # Join a game
     path("user-history/<int:user_id>/", GameUserHistoryView.as_view(), name="user-histoty"),   # list of games played by user_id
-    path("delete/<int:game_id>/", GameDeleteView.as_view(), name="game-delete"),   # Delete one game
     path("tournament/create/", TournamentCreateView.as_view(), name="tournament-create"),   # Create tournament
     path("tournament/details/<int:tournament_id>/", TournamentDetailView.as_view(), name="tournament-detail"),   # List all tournaments
     path("tournament/join/<int:tournament_id>/", TournamentJoinView.as_view(), name="tournament-join"),  # Join a tournament
-    path("tournament/delete/<int:tournament_id>/", TournamentDeleteView.as_view(), name="tournament-delete"),   # Delete a tournament
 
     # TODO remove this
-    path("delete-all/", GameDeleteAllView.as_view(), name="game-delete-all"),   # Delete all games
-    path("tournament/delete-all/", TournamentDeleteAllView.as_view(), name="tournament-delete-all"),   # Delete all tournaments
-    path("list-all/", ListAllView.as_view(), name="list-all"),                # List all games
-    path("tournament/list/", TournamentListView.as_view(), name="tournament-list"),   # List all tournaments
+    # path("delete/<int:game_id>/", GameDeleteView.as_view(), name="game-delete"),   # Delete one game
+    # path("tournament/delete/<int:tournament_id>/", TournamentDeleteView.as_view(), name="tournament-delete"),   # Delete a tournament
+    # path("delete-all/", GameDeleteAllView.as_view(), name="game-delete-all"),   # Delete all games
+    # path("tournament/delete-all/", TournamentDeleteAllView.as_view(), name="tournament-delete-all"),   # Delete all tournaments
+    # path("list-all/", ListAllView.as_view(), name="list-all"),                # List all games
+    # path("tournament/list/", TournamentListView.as_view(), name="tournament-list"),   # List all tournaments
 ]
